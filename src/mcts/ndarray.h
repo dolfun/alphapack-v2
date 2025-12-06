cc0f958be1edc5a3b4b4e842e770b45836699940
dolfun <abhinav150104@gmail.com>
2025-12-07 01:53:15 +0530

    chore: refactor ndarray

--- src/mcts/ndarray.h
+++ src/mcts/ndarray.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <type_traits>
+#include <utility>
 
 namespace mcts {
 
@@ -9,20 +10,7 @@ template <typename T, size_t... Dimensions>
 class NdArray {
 public:
   static constexpr size_t ndims = sizeof...(Dimensions);
-
   static constexpr std::array dims { Dimensions... };
-
-  static constexpr auto strides = [] {
-    std::array<size_t, ndims> strides {};
-
-    strides[ndims - 1] = 1;
-    for (size_t i = ndims - 1; i > 0; --i) {
-      strides[i - 1] = strides[i] * dims[i];
-    }
-
-    return strides;
-  }();
-
   static constexpr size_t size = (Dimensions * ...);
   static constexpr size_t bytes = sizeof(T) * size;
 
@@ -36,17 +24,39 @@ public:
     requires(sizeof...(Indices) == ndims && (std::is_integral_v<Indices> && ...))
   constexpr inline auto operator[](this auto&& self, Indices... indices) noexcept
     -> decltype(auto) {
-    size_t idx = 0, dim_idx = 0;
-    ((idx += indices * strides[dim_idx++]), ...);
-    return self.m_data[idx];
+    const auto offset = compute_offset(std::make_index_sequence<ndims> {}, indices...);
+    return self.m_data[offset];
   }
 
   [[nodiscard]] constexpr inline auto data(this auto& self) noexcept {
     return self.m_data.data();
   }
 
+  [[nodiscard]] constexpr inline auto begin(this auto& self) noexcept {
+    return self.m_data.begin();
+  }
+
+  [[nodiscard]] constexpr inline auto end(this auto& self) noexcept {
+    return self.m_data.end();
+  }
+
 private:
   std::array<T, size> m_data;
+
+  static constexpr auto strides = [] consteval {
+    std::array<size_t, ndims> strides {};
+    strides[ndims - 1] = 1;
+    for (auto i = ndims - 1; i > 0; --i) {
+      strides[i - 1] = strides[i] * dims[i];
+    }
+    return strides;
+  }();
+
+  template <size_t... Is>
+  static constexpr inline auto compute_offset(std::index_sequence<Is...>, auto... indices)
+    -> size_t {
+    return ((static_cast<size_t>(indices) * strides[Is]) + ...);
+  }
 };
 
 }  // namespace mcts
